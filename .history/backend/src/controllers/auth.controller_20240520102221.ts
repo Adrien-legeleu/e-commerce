69108160ec530c691a2b80808ea77a03c6e37677
Test <adrienlegeleu@gmail.com>
2024-05-20 12:36:05 +0200

    e -cmmerce auth and crud admin finish

---
+++ .history/backend/src/controllers/auth.controller_20240520102221.ts
@@ -0,0 +1,82 @@
+import { type Request, type Response } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+import { AdminModel, IAdmin } from "../models/admin.model";
+
+export class AuthController {
+  static getTokenAdmin(admin: IAdmin) {
+    const jwtSecret = process.env.JWT_KEY ?? "";
+    if (!jwtSecret) {
+      throw new Error("jwt secret is not defined");
+    }
+    return jwt.sign(
+      {
+        adminId: admin._id,
+      },
+      jwtSecret,
+      {
+        expiresIn: "1d",
+      }
+    );
+  }
+  checkTokenAdmin(req: any, res: Response): void {
+    res.status(200).send(req.admin);
+  }
+
+  async loginAdmin(req: Request, res: Response): Promise<void> {
+    try {
+      const { email, password } = req.body;
+      if (!email || !password) {
+        res.status(401).send({
+          error: "Email or password are incorrect",
+        });
+        return;
+      }
+      const admin = await AdminModel.findOne({ email });
+      if (!admin) {
+        res.status(401).send({
+          error: "Email or password are incorrect",
+        });
+        return;
+      }
+      const isCorrectPassword = bcrypt.compareSync(password, admin.password);
+      if (!isCorrectPassword) {
+        res.status(401).send({
+          error: "Email or password are incorrect",
+        });
+      }
+      const authToken = AuthController.getTokenAdmin(admin);
+      admin.password = "";
+      res.status(200).send({ admin, authToken });
+    } catch (error: any) {
+      console.log(error);
+      res.status(500).send({
+        error: error?.message,
+      });
+    }
+  }
+
+  async registerAdmin(req: Request, res: Response): Promise<void> {
+    try {
+      const { email, password, username } = req.body;
+      if (!email || !password || !username) {
+        res.status(400).send({ error: "Misssing properties" });
+        return;
+      }
+      const salt = bcrypt.genSaltSync(10);
+      const hashPassword = bcrypt.hashSync(password, salt);
+      const admin = await AdminModel.create({
+        username,
+        email,
+        password: hashPassword,
+      });
+      const authToken = AuthController.getTokenAdmin(admin);
+      admin.password = "";
+      res.status(200).send({ admin, authToken });
+    } catch (error: any) {
+      console.log(error);
+      res.status(500).send({ error: error?.message });
+    }
+  }
+}
---
+++ .history/backend/src/models/product.model_20240520104744.ts
@@ -0,0 +1,69 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export enum ProductStatusEnum {
+  IN_STOCK = "in stock",
+  OUT_STOCK = "out stock",
+}
+
+export interface IProduct extends Document {
+  adminId: string;
+  status: ProductStatusEnum;
+  title: string;
+  desc: string;
+  price: number;
+  qte: number;
+  imgUrl: string;
+  color: string;
+  size: string;
+  deliveryDate: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const ProductSchema = new Schema<IProduct>(
+  {
+    adminId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: [ProductStatusEnum.IN_STOCK, ProductStatusEnum.OUT_STOCK],
+      default: ProductStatusEnum.IN_STOCK,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    imgUrl: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+      required: true,
+    },
+    deliveryDate: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const ProductModel = mongoose.model<IProduct>("Product", ProductSchema);
